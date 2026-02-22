001422199171559dc437a72820a0c7feb59a5c25
Kunj Maheshwari <kunjm@outlook.com>
2026-02-22 23:15:23 +0530

    12 Messages

---
+++ src/modules/messages/hooks/message.js
@@ -0,0 +1,36 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { createMessage, getMessage } from "../actions";
+import { queryKeys } from "inngest";
+
+export const prefetchMessage = async (queryClient, projectId) => {
+  await queryClient.prefetchQuery({
+    queryKey: ["messages", projectId],
+    queryFn: () => getMessage(projectId),
+    staleTime: 10000,
+  });
+};
+
+export const useGetMessages = (projectId) => {
+    return useQuery({
+        queryKey: ["messages", projectId],
+        queryFn: () => getMessage(projectId),
+        staleTime: 10000,
+        refetchInterval: (data) => {
+            return data?.length ? 5000 : false;
+        }
+    })
+}
+
+
+export const useCreateMessages = (projectId) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (value) => createMessage(value, projectId),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["messages", projectId]
+            })
+        }
+    })
+}
