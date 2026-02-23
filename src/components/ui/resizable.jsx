1f804c58e9e252a9f1a08c939ef25729c8fe7820
Kunj Maheshwari <kunjm@outlook.com>
2026-02-23 20:01:49 +0530

    13 Code And Demo View

--- src/components/ui/resizable.jsx
+++ src/components/ui/resizable.jsx
@@ -2,45 +2,47 @@
 
 import { GripVerticalIcon } from "lucide-react"
 import * as ResizablePrimitive from "react-resizable-panels"
-
 import { cn } from "@/lib/utils"
 
-function ResizablePanelGroup({
-  className,
-  ...props
-}) {
+function ResizablePanelGroup({ className, ...props }) {
   return (
     <ResizablePrimitive.Group
       data-slot="resizable-panel-group"
-      className={cn("flex h-full w-full aria-[orientation=vertical]:flex-col", className)}
-      {...props} />
+      className={cn(
+        "flex h-full w-full min-h-0 min-w-0",
+        className
+      )}
+      {...props}
+    />
   );
 }
 
-function ResizablePanel({
-  ...props
-}) {
-  return <ResizablePrimitive.Panel data-slot="resizable-panel" {...props} />;
+function ResizablePanel({ className, ...props }) {
+  return (
+    <ResizablePrimitive.Panel
+      data-slot="resizable-panel"
+      className={cn("min-h-0 min-w-0", className)}
+      {...props}
+    />
+  );
 }
 
-function ResizableHandle({
-  withHandle,
-  className,
-  ...props
-}) {
+function ResizableHandle({ withHandle, className, ...props }) {
   return (
     <ResizablePrimitive.Separator
       data-slot="resizable-handle"
       className={cn(
-        "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden aria-[orientation=horizontal]:h-px aria-[orientation=horizontal]:w-full aria-[orientation=horizontal]:after:left-0 aria-[orientation=horizontal]:after:h-1 aria-[orientation=horizontal]:after:w-full aria-[orientation=horizontal]:after:translate-x-0 aria-[orientation=horizontal]:after:-translate-y-1/2 [&[aria-orientation=horizontal]>div]:rotate-90",
+        "relative flex items-center justify-center",
+        "bg-border",
+        "w-2 cursor-col-resize", 
+        "select-none",
+        "z-50",                  
         className
       )}
-      {...props}>
+      {...props}
+    >
       {withHandle && (
-        <div
-          className="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-xs border">
-          <GripVerticalIcon className="size-2.5" />
-        </div>
+        <GripVerticalIcon className="h-4 w-4 text-muted-foreground" />
       )}
     </ResizablePrimitive.Separator>
   );
